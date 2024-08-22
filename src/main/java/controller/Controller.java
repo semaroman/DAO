68a5045a9ade9ca4ec22e269f13a4662d41f50df
semaroman <106012219+semaroman@users.noreply.github.com>
2024-08-22 22:58:32 +0300

    Update Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
